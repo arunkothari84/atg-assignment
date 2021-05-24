d04fcbb0f4d2cc69bc5fdd880859f6acf389a66e
Arun Kothari <58799086+arunkothari84@users.noreply.github.com>
2021-05-24 20:04:31 +0530

    Add files via upload

---
+++ CV.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+import cv2
+import pytesseract
+
+app = Flask(__name__)
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\arunk\OneDrive\Documents\ML, AI, DL projects\ATG assignmnet 1\venv\Lib\site-packages\pytesseract"
+
+@app.route("/predict/", methods=['POST', 'GET'])
+def prediction(file):
+    if request.method == "GET":
+        img = cv2.imread('sample.png')
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        text = pytesseract.image_to_string(img)
+        print(text)
+
+
+if __name__ == '__main__':
+    app.run()
