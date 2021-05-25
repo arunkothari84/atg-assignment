617472125d29665a3816fc7a7cc4567090d5824b
Arun Kothari <58799086+arunkothari84@users.noreply.github.com>
2021-05-25 16:17:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, request,  render_template
+from flask import Flask, request,  render_template, flash, send_from_directory
 from cv2 import imread, cvtColor, COLOR_BGR2GRAY, threshold, THRESH_OTSU, THRESH_BINARY_INV, getStructuringElement, MORPH_RECT, dilate, findContours, RETR_EXTERNAL, CHAIN_APPROX_NONE,boundingRect, rectangle
 import pytesseract
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 pytesseract.pytesseract.tesseract_cmd = r"./vendor/tesseract-ocr/bin/tesseract"
+
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/predict/", methods=['POST', 'GET'])
 def prediction():
@@ -27,9 +36,34 @@ def prediction():
 
         return text
 
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)    
+    
 @app.route('/')
 def hello_world():
-   return render_template('./index.html')
+   if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == '__main__':
     app.run(debug=False)
