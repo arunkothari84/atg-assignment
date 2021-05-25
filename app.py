1758a72c1c352bf3572d1eb73bb6ca6856409238
Arun Kothari <58799086+arunkothari84@users.noreply.github.com>
2021-05-25 16:33:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,14 +52,15 @@ def hello_world():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return str(file)
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
    return '''
     <!doctype html>
     <title>Upload new File</title>
     <h1>Upload new File</h1>
     <form method=post enctype=multipart/form-data>
       <input type=file name=file>
-      <input type=submit value=Upload>
+      <input type=submit value=Upload href='/'>   
     </form>
     '''
 
