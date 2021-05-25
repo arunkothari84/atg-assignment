d3cfb1fc98096c2ec20baa91be47ad3639d39686
Arun Kothari <58799086+arunkothari84@users.noreply.github.com>
2021-05-25 14:18:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pytesseract
 
 app = Flask(__name__)
 
-pytesseract.pytesseract.tesseract_cmd = r"/app/.apt/usr/bin/tesseract"
+pytesseract.pytesseract.tesseract_cmd = r"./vendor/tesseract-ocr/share/tessdata"
 
 @app.route("/predict/", methods=['POST', 'GET'])
 def prediction():
