9d7ca70610215179153f2ae8aa63ae74294ccc95
Arun Kothari <58799086+arunkothari84@users.noreply.github.com>
2021-05-25 15:20:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request,  render_template
-from cv2 import imread, cvtColor, COLOR_BGR2RGB, threshold, THRESH_OTSU, THRESH_BINARY_INV, getStructuringElement, MORPH_RECT, dilate, findContours, RETR_EXTERNAL, CHAIN_APPROX_NONE,boundingRect, rectangle
+from cv2 import imread, cvtColor, COLOR_BGR2GRAY, threshold, THRESH_OTSU, THRESH_BINARY_INV, getStructuringElement, MORPH_RECT, dilate, findContours, RETR_EXTERNAL, CHAIN_APPROX_NONE,boundingRect, rectangle
 import pytesseract
 
 app = Flask(__name__)
@@ -10,19 +10,20 @@ pytesseract.pytesseract.tesseract_cmd = r"./vendor/tesseract-ocr/bin/tesseract"
 def prediction():
     if request.method == "GET":
         img = imread('sample.png')
-        img = cvtColor(img, COLOR_BGR2RGB)
+        gray = cvtColor(img, COLOR_BGR2GRAY)
         ret, thresh1 = threshold(gray, 0, 255, THRESH_OTSU | THRESH_BINARY_INV)
         rect_kernel = getStructuringElement(MORPH_RECT, (18, 18))
         dilation = dilate(thresh1, rect_kernel, iterations = 1)
         contours, hierarchy = findContours(dilation, RETR_EXTERNAL, 
                                                  CHAIN_APPROX_NONE)
+        im2 = img.copy()
         for cnt in contours:
             x, y, w, h = boundingRect(cnt)
             rect = rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cropped = im2[y:y + h, x:x + w]
             file = open("recognized.txt", "a")
             text = pytesseract.image_to_string(cropped) + "\n"
-        im2 = img.copy()
+
         return text
 
 @app.route('/')
